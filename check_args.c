fed922b441e4ff8efda5e1b4fc62a92daa395a73
Maxime Pajot-Trohel <mpajot-t@c3r4p8.42perpignan.fr>
2025-08-25 11:24:46 +0200

    modif nom fichier + ajout init des structs

---
+++ check_args.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpajot-t <mpajot-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 11:17:01 by mpajot-t          #+#    #+#             */
+/*   Updated: 2025/08/25 10:57:45 by mpajot-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/philosophers.h"
+
+int	ft_atoi(char *nptr)
+{
+	int	sign;
+	int	i;
+	int	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t'
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		num = num * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int	check_args(char *args)
+{
+	int	i;
+
+	i = 0;
+    while (args[i])
+    {
+		if (args[i] < '0' && args[i] > '9')
+			return (1);
+		i++;
+    }
+	return (0);
+}
+
+int	check_params(char **args)
+{
+	if (args[1] <= 0 || check_args(args[1]) == 1)
+		return(write(2, "Philo numbers is invalid", 25), 1);
+	else if (args[2] <= 0 || check_args(args[2] == 1))
+		return(write(2, "Time to die is invalid", 23), 1);
+	else if (args[3] <= 0 || check_args(args[3] == 1))
+		return(write(2, "Time to eat is invalid", 23), 1);
+	else if (args[4] <= 0 || check_args(args[4] == 1))
+		return(write(2, "Time to sleep is invalid", 23), 1);
+	else if (args[5] && args[5] <= 0 || check_args(args[5] == 1))
+		return(write(2, "Number of times each philo must eat is invalid", 47), 1);
+}
---
+++ init_data.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpajot-t <mpajot-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 10:41:04 by mpajot-t          #+#    #+#             */
+/*   Updated: 2025/08/25 11:19:15 by mpajot-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/philosophers.h"
+
+void	initialize_data(t_data *program, t_philo *philos)
+{
+	program->dead_flag = 0;
+	program->philos = philos;
+	pthread_mutex_init(&program->dead_lock, NULL);
+	pthread_mutex_init(&program->write_lock, NULL);
+	pthread_mutex_init(&program->meal_lock, NULL);
+}
+
+static void	init_args(t_philo *philos, char **args)
+{
+	philos->num_of_philos = args[1];
+	philos->time_to_die = args[2];
+	philos->time_to_eat = args[3];
+	philos->time_to_sleep = args[4];
+	if (args[5])
+		philos->num_to_eat = args[5];
+	else
+		philos->num_to_eat = 0;
+}
+
+void	initialize_philos(t_philo *philos, pthread_mutex_t *forks, t_data *program, char **args)
+{
+	int	i;
+
+	i = 0;
+	while (i < ft_atoi(args[1]))
+	{
+		philos[i].phil_id = i;
+		philos[i].write_lock = &program->write_lock;
+		philos[i].dead_lock = &program->dead_lock;
+		philos[i].meal_lock = &program->meal_lock;
+		philos[i].meals_eaten = 0;
+		philos[i].eating = 0;
+		philos[i].start_time = get_current_time();
+		philos[i].last_meal = get_current_time();
+		philos[i].dead = &program->dead_flag;
+		philos[i].left_fork = &forks[i];
+		init_args(&philos[i], args);
+		if (i == 0)
+			philos[i].right_fork = &forks[philos[i].num_of_philos - 1];
+		else
+			philos[i].right_fork = &forks[i - 1];
+	}
+}
