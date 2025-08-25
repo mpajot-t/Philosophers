bd9d286ed4500d3cbbb389be1a53726c2471b770
Maxime Pajot-Trohel <mpajot-t@c3r4p8.42perpignan.fr>
2025-08-25 11:25:11 +0200

    prototype fonctions

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,29 +6,54 @@
 /*   By: mpajot-t <mpajot-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 10:23:40 by mpajot-t          #+#    #+#             */
-/*   Updated: 2025/08/21 12:46:29 by mpajot-t         ###   ########.fr       */
+/*   Updated: 2025/08/25 11:19:45 by mpajot-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# define MAX_PHILO 200
+
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include <pthread.h>
+# include <sys/time.h>
 
 typedef struct s_philo
 {
-	pthread_t		thread;
-	int				phil_id;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				num_to_eat;
-	pthread_mutex_t	right_fork;
-	pthread_mutex_t	left_fork;
-	
+	pthread_t			thread;
+	int					*dead;
+	int					num_of_philos;
+	int					meals_eaten;
+	int					eating;
+	int					last_meal;
+	int					phil_id;
+	size_t				start_time;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	int					num_to_eat;
+	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*write_lock;
+	pthread_mutex_t		*dead_lock;
+	pthread_mutex_t		*meal_lock;
 }	t_philo;
 
-int	check_args(char **args);
+typedef struct s_data
+{
+	t_philo			*philos;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	int				dead_flag;
+	
+}	t_data;
+
+int		check_args(char **args);
+int		ft_atoi(char *nptr);
+void	initialize_data(t_data *program, t_philo *philos);
+void	initialize_philos(t_philo *philos, pthread_mutex_t *forks, t_data *program, char **args);
 #endif
