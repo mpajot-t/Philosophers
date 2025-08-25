05e5759aaa0dae098227aad6aacbead301c76568
Maxime Pajot-Trohel <mpajot-t@c3r4p8.42perpignan.fr>
2025-08-25 11:26:54 +0200

    ajout appel init

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mpajot-t <mpajot-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 10:23:58 by mpajot-t          #+#    #+#             */
-/*   Updated: 2025/08/21 11:23:14 by mpajot-t         ###   ########.fr       */
+/*   Updated: 2025/08/25 11:26:29 by mpajot-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int main(int argc, char **argv)
 {
+	t_data			program;
+	t_philo			philos[MAX_PHILO];
+	pthread_mutex_t	forks[MAX_PHILO];
+
 	if (argc < 5 || argc > 6)
 		return (0);
 	if (check_args(argv) == 0)
 		return (0);
-	else
-		printf("hey");
-	return (0);
+	initialize_philos(philos, forks, &program, argv);
+	initialize_data(&program, philos);
 }
